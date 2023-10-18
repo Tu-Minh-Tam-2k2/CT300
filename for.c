a84e82665893d76198073d96f5d5f19933e5d084
Tu-Minh-Tam-2k2 <tamb2014784@student.ctu.edu.vn>
2023-10-18 19:51:42 +0700

    chuong trinh c

---
+++ for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  // Khai báo biến đếm
+  int i = 1;
+
+  // Vòng lặp for
+  for (i = 1; i <= 10; i++) {
+    // Hiển thị số đếm
+    printf("%d\n", i);
+  }
+  return 0;
+}
